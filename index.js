927b626dea97c50b6e63d1d013f8323f430287ad
Joseph Dykstra <josephdykstra@gmail.com>
2014-03-18 18:34:41 -0500

    Implemented callback(s)

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ var stream = require("stream")
 var priceFromHash = require("price-from-hash.js")
 
 module.exports = function getStl(callback) {
-	http.createConnection(request, reqStr) { //make this code correcter
+	http.createConnection(function(request, reqStr) { //make this code correcter
 		if (request === "post") {
 			var hash = ""
 			if (reqStr.split("/")[1] === "stl") {
 				hash = reqStr.split("/")[2]
+				priceFromHash(hash, callback)
 			} else {
 				//get the file
-				//save to disc
-				//use hasher to hash it
-				hash = "qwertyuiopasdfghjklzxcvbnmjoseph"
+					//save to disc
+						//use hasher to hash it
+						hash = "qwertyuiopasdfghjklzxcvbnmjoseph"
+						priceFromHash(hash, callback)
 			}
-			priceFromHash(hash, callback)
 		}
-	}
+	})
 }
--- price-from-hash.js
+++ price-from-hash.js
@@ -3,5 +3,10 @@ var Database = require("..\omaha-3d-print-database\mock.js")
 var db = Database()
 
 module.exports = function priceFromHash(hash, callback) {
-	db.get(hash)
+	db.get(hash, function(result) {
+		if (if result != null && typeof result === "object") {
+			
+		}
+	})
+	
 }
