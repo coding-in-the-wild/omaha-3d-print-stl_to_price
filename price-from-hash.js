339e85b776c18eed3f8086c7087d77b23e99d2fa
Joseph Dykstra <josephdykstra@gmail.com>
2014-03-19 16:05:57 -0500

    Fixed price-from-hash.js and added testPriceFromHash.js

--- price-from-hash.js
+++ price-from-hash.js
@@ -1,12 +1,15 @@
+var getPrice = require("../omaha-3d-print-price_calculator/getPrice.js")
 
-var Database = require("..\omaha-3d-print-database\mock.js")
-var db = Database()
-
-module.exports = function priceFromHash(hash, callback) {
-	db.get(hash, function(result) {
-		if (if result != null && typeof result === "object") {
-			
-		}
-	})
-	
+module.exports = function PriceFromHash(database) {
+	return function priceFromHash(hash, callback) {
+		database.get(hash, function(err, result) {
+			if (!err) {
+				if (result != null && typeof result === "object") {
+					callback( getPrice({}, result) )
+				} else {
+					callback( new Error("'get' did not return an object") )
+				}
+			}
+		})
+	}
 }
---
+++ test/testPriceFromHash.js
@@ -0,0 +1,21 @@
+﻿var test = require('tap').test
+var Database = require("../../omaha-3d-print-database/mock.js")
+var db = new Database()
+var PriceFromHash = require("../price-from-hash.js")
+
+var fakeHash = "Md5HashesAre32CharactersInLength"
+var fakeObj = { volume: 16387 }
+
+test("test getting a price from a hash", function test1(t) {
+	t.plan(2)
+	var priceFromHash = new PriceFromHash(db)
+	db.insert(fakeHash, fakeObj, function doneInserting(err) {
+		if (!err) {
+			priceFromHash(fakeHash, function cbGetPrice2(price) {
+				t.ok(price, "price is truthy")
+				t.equal(price, 13.603174999999998, "returns correct price")
+				t.end()
+			})
+		}
+	})
+})
