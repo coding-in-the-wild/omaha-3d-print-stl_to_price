be5eebe4d8dde75ccc2b6824ef4780a6e7eaf5e3
Joseph Dykstra <josephdykstra@gmail.com>
2014-03-24 18:32:43 -0500

    Refactored comments in index.js

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ var stream = require("stream")
 var url = require('url')
 
 // App variables
-var file_url = 'http://upload.wikimedia.org/wikipedia/commons/4/4f/Big%26Small_edit_1.jpg';
+var file_url = 'http://upload.wikimedia.org/wikipedia/commons/4/4f/Big%26Small_edit_1.jpg'
 var FILE_PATH = './stl_files/'
-
+//In the future, use this: github.com/substack/rolling-hash ?
 
 
 module.exports = function getStl(callback) {
@@ -21,34 +21,27 @@ module.exports = function getStl(callback) {
 				hash = reqStr.split("/")[2]
 				priceFromHash(hash, callback)
 			} else {
-				//get the file
-				var download_file_httpget = function(file_url) {
-					var options = {
-						host: url.parse(file_url).host,
-						port: 80,
-						path: url.parse(file_url).pathname
-					}
-					
-					var file_name = url.parse(file_url).pathname.split('/').pop();
-					var file = fs.createWriteStream(FILE_PATH + file_name);
-
-					http.get(options, function(res) {
-						res.on('data', function(data) {
-							file.write(data)
-							md5.update(data)
-						}).on('end', function() {
-							file.end()
-							hash = md5.digest('hex')
-							console.log(file_name + ' downloaded to ' + FILE_PATH)
-						});
-					});
+				var options = {
+					host: url.parse(file_url).host,
+					port: 80,
+					path: url.parse(file_url).pathname
 				}
-					//save to disc
-						//use hasher to hash it
-						//In the future, use this: github.com/substack/rolling-hash
-						hash = "qwertyuiopasdfghjklzxcvbnmjoseph"
+				
+				var file_name = url.parse(file_url).pathname.split('/').pop()
+				var file = fs.createWriteStream(FILE_PATH + file_name)
+
+				http.get(options, function(res) { //get the file
+					res.on('data', function(data) {
+						file.write(data) //save to disc
+						md5.update(data)
+					}).on('end', function() {
+						file.end()
+						hash = md5.digest('hex') //use hasher to hash it
+						console.log(file_name + ' downloaded to ' + FILE_PATH)
 						priceFromHash(hash, callback)
+					})
+				})
 			}
 		}
-	})
-}
+	}
+})
