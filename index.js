d7332fad22c36ab176703626d6a9c4985583a75e
Joseph Dykstra <josephdykstra@gmail.com>
2014-03-25 14:35:59 -0500

    Updated index.js

--- index.js
+++ index.js
@@ -3,31 +3,31 @@ var spawn = require('child_process').spawn
 var md5 = require('crypto').createHash('md5')
 var fs = require('fs')
 var http = require("http")
-var priceFromHash = require("price-from-hash.js")
+var priceFromHash = require("./priceFromHash.js")
 var stream = require("stream")
 var url = require('url')
 
-// App variables
-var file_url = 'http://upload.wikimedia.org/wikipedia/commons/4/4f/Big%26Small_edit_1.jpg'
-var FILE_PATH = './stl_files/'
-//In the future, use this: github.com/substack/rolling-hash ?
-
-
 module.exports = function getStl(callback) {
-	http.createConnection(function(request, reqStr) { //make this code correcter
-		if (request === "post") {
+	var FILE_PATH = './stl_files/'
+	var options = {
+		hostname: '192.168.0.1',
+		port: 8080,
+		path: '/upload',
+		method: 'POST'
+	}
+	
+	http.request(options, function(res) {
+		//idk
+	}).on('post', function parse() {
+		console.log("req: "+req)
+		if (req === "post") {
 			var hash = ""
-			if (reqStr.split("/")[1] === "stl") {
-				hash = reqStr.split("/")[2]
+			url.parse(reqStr).pathname.split("/")
+			if (url.parse(reqStr).pathname.split("/")[1] === "stl") {
+				hash = url.parse(reqStr).pathname.split("/").pop()
 				priceFromHash(hash, callback)
 			} else {
-				var options = {
-					host: url.parse(file_url).host,
-					port: 80,
-					path: url.parse(file_url).pathname
-				}
-				
-				var file_name = url.parse(file_url).pathname.split('/').pop()
+				var file_name = 'thefilenamecomesrightfromthehash'
 				var file = fs.createWriteStream(FILE_PATH + file_name)
 
 				http.get(options, function(res) { //get the file
@@ -37,11 +37,16 @@ module.exports = function getStl(callback) {
 					}).on('end', function() {
 						file.end()
 						hash = md5.digest('hex') //use hasher to hash it
-						console.log(file_name + ' downloaded to ' + FILE_PATH)
+						console.log(file_name + ' downloaded to ' + FILE_PATH +
+							'and has this hash: ' + hash)
 						priceFromHash(hash, callback)
 					})
 				})
 			}
 		}
-	}
-})
+	})
+}
+
+
+
+//var file_name = url.parse(file_url).pathname.split('/').pop()
