16261237897a36c5d91ea0e78aeccda7f1b65b77
Joseph Dykstra <josephdykstra@gmail.com>
2014-04-10 18:09:13 -0500

    Updated server things, still under development...

---
+++ httpTest.js
@@ -0,0 +1,52 @@
+﻿var http = require('http')
+var fs = require('fs');
+
+var port = 8080
+var method = 'POST' //'GET'
+
+//////////
+//SERVER//
+//////////
+http.createServer().on('connection', function(socket) {
+	console.log('connection')
+	console.log('')
+}).on('connect', function(req, socket, head) {
+	console.log('connect')
+	console.log('')
+}).on('request', function (req, res) {
+	console.log('request')
+	console.log('')
+}).on('checkContinue',  function (req, res) {
+	console.log('checkContinue')
+	console.log('')
+}).on('upgrade', function() {
+	console.log('upgrade')
+	console.log('')
+}).on('clientError', function() {
+	console.log('clientError')
+	console.log('')
+}).on('close', function() {
+	console.log('close')
+	console.log('')
+}).listen(port, function(){
+	console.log("server started")
+})
+
+//////////
+//CLIENT//
+//////////
+/*var opts = {
+	port: port,
+	path: 'server/stl/',
+	method: 'CONNECT'
+}
+http.request(opts, function(res2) {
+	console.log("res2")
+	console.dir(res2)
+	res.setEncoding('utf8');
+	res.on('data', function (chunk) {
+		console.log('BODY: ' + chunk);
+	})
+}).on('error', function(e) {
+	console.log('problem with request: ' + e.message);
+}).end()*/
--- index.js
+++ index.js
@@ -14,48 +14,37 @@ var writePath = './stl_files/'
 var ext = '.js'
 var fakeHash = 'Md5HashesAre32CharactersInLength'
 
-console.log("1")
-var rStream = http.createServer(function(req, res) {
-	console.log("4")
-	console.log("req")
-	console.dir(req)
-	console.log("res")
-	console.dir(res)
-	console.log("5")
-	
-	saveAndHashFile(rStream, writePath, ext, function(err, object) {
-		console.log("6")
+http.createServer(function(req, res) {
+	console.log("2")
+	saveAndHashFile(req, writePath, ext, function(err, object) {
+		console.log("3")
 		t.notOk(err, 'no error')
 		t.equal(object.price, 5.0175780752, 'price is correct')
 		t.equal(object.hash, 'd372818be56327b94ad912f903b33b2f', 'hash is correct')
-		console.log("7")
 	})
 }).listen(8080)
-console.log("2")
-console.dir(rStream)
-console.log("3")
+console.log("1")
 //http.request() returns an instance of the http.ClientRequest class.
 //The ClientRequest instance is a writable stream.
 //If one needs to upload a file with a POST request,
 //then write to the ClientRequest object.
 
 var options = {
-  hostname: 'www.google.com',
-  port: 80,
+  port: 8080,
   path: 'server/stl/' + fakeHash,
   method: 'POST'
 }
 
-var req = http.request(options, function(res) {
-	console.log("8")
-	console.log("i have no idea what this means")
+http.request(options, function(res) {
+	console.log("4 i have no idea what this means")
 	res.on('data', function (chunk) {
-		console.log('res BODY: ' + chunk);
-		console.log("9")
+		console.log('5 res BODY: ' + chunk);
 	})
-}).on('response', function(blah) {
-	console.log("10")
-	console.log("i have no idea what this means either")
+}).on('response', function(res) {
+	console.log("6 response")
+	console.dir(res)
+}).on('data', function() {
+	console.log("I don't think you will ever see this")
 })
 
 /*
@@ -63,16 +52,16 @@ var req = http.request(options, function(res) {
 ****eg****
 
 var req = http.request(options, function(res) {
-  console.log('STATUS: ' + res.statusCode)
-  console.log('HEADERS: ' + JSON.stringify(res.headers))
-  res.setEncoding('utf8')
-  res.on('data', function (chunk) {
-    console.log('BODY: ' + chunk)
-  })
+	console.log('STATUS: ' + res.statusCode)
+	console.log('HEADERS: ' + JSON.stringify(res.headers))
+	res.setEncoding('utf8')
+	res.on('data', function (chunk) {
+		console.log('BODY: ' + chunk)
+	})
 })
 
 req.on('error', function(e) {
-  console.log('problem with request: ' + e.message)
+	console.log('problem with request: ' + e.message)
 })
 
 // write data to request body
