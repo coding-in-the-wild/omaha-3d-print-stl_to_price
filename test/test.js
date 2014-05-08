7171dc872310dbc1db2bec7c573a92749f44cf6b
Joseph Dykstra <josephdykstra@gmail.com>
2014-05-08 17:49:44 -0500

    Updated tests

--- test/test.js
+++ test/test.js
@@ -1,23 +1,23 @@
 ﻿var http = require('http')
 
 var options = {
-  hostname: 'www.google.com',
-  port: 80,
-  path: '',
-  method: 'GET'
+	hostname: 'www.google.com',
+	port: 80,
+	path: '',
+	method: 'GET'
 }
 
 var req = http.request(options, function(res) {
-  console.log('STATUS: ' + res.statusCode)
-  console.log('HEADERS: ' + JSON.stringify(res.headers))
-  res.setEncoding('utf8')
-  res.on('data', function (chunk) {
-    console.log('BODY: ' + chunk)
-  })
+	console.log('STATUS: ' + res.statusCode)
+	console.log('HEADERS: ' + JSON.stringify(res.headers))
+	res.setEncoding('utf8')
+	res.on('data', function (chunk) {
+		console.log('BODY: ' + chunk)
+	})
 })
 
 req.on('error', function(e) {
-  console.log('problem with request: ' + e.message);
+	console.log('problem with request: ' + e.message);
 })
 
 // write data to request body
--- test/testHttp.js
+++ test/testHttp.js
@@ -1,5 +1,6 @@
-﻿/*var test = require('tap').test
-var http = require('http')
+﻿var test = require('tap').test
+var request = require('supertest')
+var express = require('express')
 var saveAndHashFile = require('../index.js')
 var file = '../stl_files/companion-cube-2.stl'
 var obj = {
@@ -7,14 +8,16 @@ var obj = {
 	ext: '.stl',
 	tempPath: '../stl_files/temp.stl'
 }
-var stream = http.get('http://www.thingiverse.com/download:95619')
+
 
 test("test getting a price from a hash", function test1(t) {
 	t.plan(3)
+	var app = express()
+	app.listen(8080)
+	
+	request(app)
+		.post('/d372818be56327b94ad912f903b33b2f')
+		.attach('avatar', file)
 	saveAndHashFile(stream, obj, function(err, object) {
-		console.log(err)
-		t.notOk(err, 'no error')
-		t.equal(object.price, 5.0175780752, 'price is correct')
-		t.equal(object.hash, 'd372818be56327b94ad912f903b33b2f', 'hash is correct')
 	})
-})*/
+})
